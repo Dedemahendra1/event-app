3d84d2735e96fc830d0a73e17b408199cff9339f
Dede <dedemahendra201@gmail.com>
2024-01-12 22:16:48 +0700

    implementing stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import Collection from "@/components/shared/Collection";
+import { SearchParamProps } from "@/types";
+import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -34,6 +48,16 @@ export default function Home() {
           Search
           category
         </div>
+
+        <Collection 
+        data={events?.data}
+        emptyTitle="No Events Found"
+        emptyStateSubtext="Come back later"
+        collectionType="All_Events"
+        limit={6}
+        page={1}
+        totalPages={2}
+        />
       </section>
     </>
   )
